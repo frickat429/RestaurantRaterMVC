2b0220bbd20054a0ee4027c41ba78e14d738c146
Austin Frick <austin.frick429@gmail.com>
2024-07-18 19:44:55 -0400

    rating view update with componets

---
+++ ViewComponents/RestaurantRatings.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using RestaurantRaterMVC.Services.Ratings;
+
+namespace RestaurantRaterMVC.ViewComponents;
+
+[ViewComponent(Name = nameof(RestaurantRatings))]
+public class RestaurantRatings : ViewComponent
+{
+    private readonly IRatingService _ratingService;
+    public RestaurantRatings(IRatingService ratingService) => _ratingService = ratingService;
+
+    public async Task<IViewComponentResult> InvokeAsync(int restaurantId)
+    {
+        var ratings = await _ratingService.GetRestaurantRatingsAsync(restaurantId);
+        return View(ratings);
+    }
+}
